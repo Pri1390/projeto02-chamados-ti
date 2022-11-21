504caa176ba7622d65149b9d24b28acaa141f534
Priscila Vieira <freedom.pva@gmail.com>
2022-11-21 16:42:47 -0300

    incluindo search bar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import { Table, Container, Button } from "react-bootstrap"
+import { Table, Container, Button, Form, FloatingLabel } from "react-bootstrap"
 import { useEffect,useState } from "react"
 import axios from "axios";
 import ModalCreateUser from "../components/ModalCreateUser";
@@ -7,10 +7,9 @@ import { Link } from "react-router-dom";
 
 function HomePage() {
 
-    const [users, setUsers] = useState ([])
-
-    
+    const [users, setUsers] = useState ([])    
     const [reload, setReload] = useState(false);
+    const [search, setSearch] = useState("");
 
     useEffect (() =>{
 
@@ -20,52 +19,75 @@ function HomePage() {
         }
 
         fetchUsers()
-
     },[reload])
 
-    console.log(users)
+  
+    function handleSearch(e) {
+      setSearch(e.target.value);
+    }
+
+    console.log(search)
 
 
-    return ( 
-        <div>
-            <h1>HomePage</h1>
-        
-            <Container>
-            <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Nome do Solicitante</th>
-          <th>Matrícula</th>
-          <th>Setor</th>
-          <th>Solicitação</th>
-          <th>Detalhes</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user) =>{
-            return(
-              <tr key={user._id}>
-                  <td>{user.nome}</td>
-                  <td>{user.matricula}</td>
-                  <td>{user.setor}</td>
-                  <td>{user.solicitacao}</td>                  
-                  <td>
-                  <Link to={`/user/${user._id}`}>
-                  <Button variant="outline-primary">Detalhes</Button>
-                  </Link>
-                  
-                  </td>
-                  </tr> 
-            )
-        })}             
-      </tbody>
-     </Table>
-        
-     <ModalCreateUser reload={reload} setReload={setReload} />
-        
-    </Container> 
-   </div>
-  );
+    return (
+      <div>
+        <h1>HomePage</h1>
+
+        <Container>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Pesquise por nome ou matrícula"
+            className="my-3"
+          >
+            <Form.Control
+              type="text"
+              placeholder="pesquise"
+              value={search}
+              onChange={handleSearch}
+            />
+          </FloatingLabel>
+
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Nome do Solicitante</th>
+                <th>Matrícula</th>
+                <th>Setor</th>
+                <th>Solicitação</th>
+                <th>Detalhes</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users
+              .filter((user) => {
+                return (
+                  user.nome.toLowerCase().includes(search.toLowerCase()) ||
+                  user.matricula.toLowerCase().includes(search.toLowerCase())
+                ) 
+                
+              })
+              .map((user) => {
+                return (
+                  <tr key={user._id}>
+                    <td>{user.nome}</td>
+                    <td>{user.matricula}</td>
+                    <td>{user.setor}</td>
+                    <td>{user.solicitacao}</td>
+                    <td>
+                      <Link to={`/user/${user._id}`}>
+                        <Button variant="outline-primary">Detalhes</Button>
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+
+          <ModalCreateUser reload={reload} setReload={setReload} />
+        </Container>
+      </div>
+    );
 }
 
 export default HomePage;
