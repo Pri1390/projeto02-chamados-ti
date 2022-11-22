b0489e2acf0608d9c83f61126891785e16bcea42
Priscila Vieira <freedom.pva@gmail.com>
2022-11-22 18:57:21 -0300

    inclusão de imagem e form na tela de login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,18 +1,44 @@
-import { Button, Container } from "react-bootstrap"
+import { Button, Container,Form,Row } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 // -------- COMPONENTE INICIAL --------
 // ao entrar na aplicação será mostrado este componente para o usuário iniciar sua navegação na página
 function LoginPage() {
-    return (
-        <Container style={{ height: '100vh' }} className="d-flex align-items-center justify-content-center">
-            <Button className="p-4" variant="dark" size="lg">
-                <Link className="nav-link" to="/HomePage">
-                   Entrar no sistema
-                </Link>
-            </Button>
-        </Container>
-    )
+  return (
+    <Container
+      style={{ height: "90vh" }}
+      className="d-flex align-items-center justify-content-center"
+    >
+      <Form>
+        <Row>
+          <img
+            className="chamados-imagem"
+            src="https://esales.com.br/wp-content/uploads/2019/11/quais-os-indicadores-para-otimizacao-de-atendimento-online.jpg"
+            alt="imagem chamado ti"
+            width="500"
+            height="300"
+          />
+        </Row>
+        <Row>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Acesso ao sistema</Form.Label>
+            <Form.Control type="email" placeholder="Usuário" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="Senha" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Lembrar senha" />
+          </Form.Group>
+          <Button className="p-2" variant="dark" size="lg">
+            <Link className="nav-link" to="/HomePage">
+              Acessar
+            </Link>
+          </Button>
+        </Row>
+      </Form>
+    </Container>
+  );
 }
 
 export default LoginPage
