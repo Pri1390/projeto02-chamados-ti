6c8107d8afbda8ed18733c709de4f14af50fd5c5
Priscila Vieira <freedom.pva@gmail.com>
2022-11-23 15:05:05 -0300

    atualziando

--- src/components/MigalhadePao.js
+++ src/components/MigalhadePao.js
@@ -3,7 +3,7 @@ import Breadcrumb from 'react-bootstrap/Breadcrumb';
 function BreadcrumbEx() {
   return (
     <Breadcrumb>
-      <Breadcrumb.Item href="#">Tela de Login</Breadcrumb.Item>
+      <Breadcrumb.Item href="#"> Tela de Login</Breadcrumb.Item>
       <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
         Controle de Chamados
       </Breadcrumb.Item>
